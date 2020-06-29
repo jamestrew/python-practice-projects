9bc1065b99c613d2da5b502c8bdcc7f8e109a786
trewjames <j.trew10@gmail.com>
2020-06-29 10:17:25 -0400

    2048 alpha

--- tkinter/2048/2048.py
+++ tkinter/2048/2048.py
@@ -113,7 +113,7 @@ class Game(tk.Frame):
         new_matrix = [[0] * 4 for _ in range(4)]
         for i in range(4):
             for j in range(4):
-                new_matrix[i][j] = self.matrix[i][j]
+                new_matrix[i][j] = self.matrix[j][i]
         self.matrix = new_matrix
 
     def add_new_tiles(self):
@@ -130,15 +130,16 @@ class Game(tk.Frame):
                 cell_value = self.matrix[i][j]
                 if cell_value == 0:
                     self.cells[i][j]["frame"].configure(bg=c.EMPTY_CELL_COLOR)
-                    self.cells[i][j]["number"].configure(bg=c.EMPTY_CELL_COLOR, text='')
+                    self.cells[i][j]["number"].configure(
+                        bg=c.EMPTY_CELL_COLOR, text='')
                 else:
-                    self.cells[i][j]["frame"].configure(bg=c.CELL_COLORS[cell_value])
+                    self.cells[i][j]["frame"].configure(
+                        bg=c.CELL_COLORS[cell_value])
                     self.cells[i][j]["number"].configure(
                         bg=c.CELL_COLORS[cell_value],
                         fg=c.CELL_NUMBER_COLORS[cell_value],
                         font=c.CELL_NUMBER_FONTS[cell_value],
-                        text=str(cell_value)
-                    )
+                        text=str(cell_value))
         self.score_label.configure(text=self.score)
         self.update_idletasks()
 
@@ -148,6 +149,7 @@ class Game(tk.Frame):
         self.stack()
         self.add_new_tiles()
         self.update_GUI()
+        self.game_over()
 
     def right(self, event):
         self.reverse()
@@ -157,6 +159,7 @@ class Game(tk.Frame):
         self.reverse()
         self.add_new_tiles()
         self.update_GUI()
+        self.game_over()
 
     def up(self, event):
         self.transpose()
@@ -166,6 +169,7 @@ class Game(tk.Frame):
         self.transpose()
         self.add_new_tiles()
         self.update_GUI()
+        self.game_over()
 
     def down(self, event):
         self.transpose()
@@ -177,6 +181,48 @@ class Game(tk.Frame):
         self.transpose()
         self.add_new_tiles()
         self.update_GUI()
+        self.game_over()
+
+    def horizontal_move_exists(self):
+        for i in range(4):
+            for j in range(3):
+                if self.matrix[i][j] == self.matrix[i][j + 1]:
+                    return True
+        return False
+
+    def vertical_move_exists(self):
+        for i in range(3):
+            for j in range(4):
+                if self.matrix[i][j] == self.matrix[i + 1][j]:
+                    return True
+        return False
+
+    def game_over(self):
+        if any(2048 in row for row in self.matrix):
+            game_over_frame = tk.Frame(self.main_grid, borderwidth=2)
+            game_over_frame.place(relx=0.5, rely=0.5, anchor="center")
+            tk.Label(
+                game_over_frame,
+                text="You Win!",
+                bg=c.WINNER_BG,
+                fg=c.GAME_OVER_FONT_COLOR,
+                font=c.GAME_OVER_FONT
+            ).pack()
+        elif any(0 in row for row in self.matrix) and not self.horizontal_move_exists() and not self.vertical_move_exists():
+            game_over_frame = tk.Frame(self.main_grid, borderwidth=2)
+            game_over_frame.place(relx=0.5, rely=0.5, anchor="center")
+            tk.Label(
+                game_over_frame,
+                text="You Lose!",
+                bg=c.LOSER_BG,
+                fg=c.GAME_OVER_FONT_COLOR,
+                font=c.GAME_OVER_FONT
+            ).pack()
 
 
-Game()
+def main():
+    Game()
+
+
+if __name__ == "__main__":
+    main()
