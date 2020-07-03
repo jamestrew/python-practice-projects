9fa1b31443b125c4197a499fc2598791c0bd98e3
trewjames <j.trew10@gmail.com>
2020-07-03 17:21:41 -0400

    complete 7/3 practice

--- study/linked_list.py
+++ study/linked_list.py
@@ -19,10 +19,23 @@ class Solution():
             print(current.data, end=' ')
             current = current.next
 
+    def removeDuplicates(self, head):
+        current = head
+
+        while current.next:
+            if current.data != current.next.data:
+                current = current.next
+            else:
+                current.next = current.next.next
+        return head
+
 
 mylist = Solution()
-sample = [2, 3, 4, 1]
+sample = [1, 2, 2, 3, 3, 4]
 head = None
 for data in sample:
     head = mylist.insert(head, data)
+mylist.display(head)
+print()
+head = mylist.removeDuplicates(head)
 mylist.display(head)
--- test.py
+++ test.py
@@ -1,3 +1,18 @@
-lst = [2]
-lst.insert(0, 5)
-print(lst)
+def calculateFine(ret, due):
+    # same before or before: Fine = 0
+    # same month but late: Fine = 15 * # days late
+    # same year but late: Fune = 500 * # months late
+    # late different year: Fine = 10000
+    if ret <= due:
+        return 0
+    if ret[0] > due[0]:
+        return 10000
+    if ret[1] > due[1]:
+        return 500 * (ret[1] - due[1])
+    if ret[2] > due[2]:
+        return 15 * (ret[2] - due[2])
+
+
+ret = (2015, 6, 9)
+due = (2015, 6, 6)
+print(calculateFine(ret, due))
