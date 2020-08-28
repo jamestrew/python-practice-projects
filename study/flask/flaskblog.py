b7d52bd2a3f2c2daf862de28c4404a01bdefc7e4
trewjames <j.trew10@gmail.com>
2020-08-28 09:49:40 -0400

    Add login and registration forms.

--- study/flask/flaskblog.py
+++ study/flask/flaskblog.py
@@ -1,5 +1,10 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, flash, redirect, url_for
+
+from forms import RegistrationForm, LoginForm
+
 app = Flask(__name__)
+
+app.config['SECRET_KEY'] = 'f41f71d0e0d6d8b176dd086200a16e43'
 
 posts = [
     {
@@ -26,6 +31,25 @@ def home():
 @app.route("/about")
 def about():
     return render_template('about.html', title='About')
+
+
+@app.route("/register", methods=['GET', 'POST'])
+def register():
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        flash(f'Account created for {form.username.data}!', 'success')
+        return redirect(url_for('home'))
+
+    return render_template('register.html', title='Register', form=form)
+
+
+@app.route("/login", methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        flash(f'Welcome back!', 'success')
+        return redirect(url_for('home'))
+    return render_template('login.html', title='Login', form=form)
 
 
 if __name__ == "__main__":
